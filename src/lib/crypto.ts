5c3aadfd8461a69e0bbd5ecabde0850614a45804
JiHoon-0330 <ljihoon0330@gamil.com>
2023-05-20 13:42:23 +0900

    encrypt stringify 변경

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -1,9 +1,10 @@
 import { Provider } from "@/app/auth/[provider]/page";
 import CryptoJS from "crypto-js";
+import stringify from "fast-json-stable-stringify";
 
 export function encrypt(params: { email: string; provider: Provider }) {
   return CryptoJS.AES.encrypt(
-    JSON.stringify(params),
+    stringify(params),
     process.env.NEXT_PUBLIC_SHA_KEY,
   ).toString();
 }
